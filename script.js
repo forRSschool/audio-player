5d47fe5fb4b91e34e4f6b2af5f882db90dc59b0c
Shahriyor <shahriyoradhamov@icloud.com>
2024-08-29 22:47:29 +0500

    fix: fix bugs with duration range

--- script.js
+++ script.js
@@ -5,12 +5,39 @@ const volumeBtn = document.querySelector('.volume-btn');
 const volumeImg = document.querySelector('.volume-img');
 const volumeInput = document.querySelector('.volume-input');
 const durationInput = document.querySelector('.duration-input');
+const btnPrev = document.querySelector('#btnPrev');
+const btnNext = document.querySelector('#btnNext');
 
 let isPlayMusic = false;
 let isMusicVolumeOn = true;
+let trackId = 0;
+
+const tracks = [
+  'Immortal',
+  'No Talk',
+  'Enough',
+  'Skylines',
+  'Get Trough',
+  'Lofi Mallet',
+  'Winning'
+]
+
+const artists = [
+  'NEFFEX',
+  'VYEN',
+  'NEFFEX',
+  'Anno Domini Beats',
+  'NEFFEX',
+  'Kwon',
+  'NEFFEX'
+]
+
+btnPrev.addEventListener('click', prevMusic);
+btnNext.addEventListener('click', nextMusic);
 
 mainBtn.addEventListener('click', () => {
   isPlayMusic? musicPause() : musicPlay();
+  isPlayMusic = !isPlayMusic;
 })
 
 volumeBtn.addEventListener('click', () => {
@@ -28,13 +55,15 @@ volumeInput.addEventListener("input", (event) => {
 
 function musicPlay() {
   audio.play();
-  isPlayMusic = true;
   mainBtnImg.src = 'assets/icons/pause.png'
+  setInterval(() => {
+    durationInput.value = audio.currentTime;
+  }, 500)
+  
 }
 
 function musicPause() {
   audio.pause();
-  isPlayMusic = false;
   mainBtnImg.src = 'assets/icons/play.png'
 }
 
@@ -50,17 +79,29 @@ function musicVolumeOn() {
   volumeImg.src = 'assets/icons/volume_up.png'
 }
 
+function nextMusic() {
+  trackId++;
+  console.log(trackId)
+}
+
+function prevMusic() {
+  trackId--;
+  console.log(trackId)
+}
+
 audio.onloadedmetadata = function() {
   durationInput.max = audio.duration;
   durationInput.value = audio.currentTime;
+
 }
 
-if(audio.play()) {
+
+
+durationInput.addEventListener('input', () => {
+  audio.currentTime = durationInput.value;
+  if(isPlayMusic) {
   setInterval(() => {
     durationInput.value = audio.currentTime;
   }, 500)
 }
-
-durationInput.addEventListener('input', () => {
-  audio.currentTime = durationInput.value;
 })
